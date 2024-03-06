52d66bf35db15f1d02b792d53093a34782bc25c5
Michael Gafah <michaelgafah@gmail.com>
2024-03-06 20:10:25 +0000

    adds projects and contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import About from "./components/About";
+import Contact from "./components/Contact";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import Projects from "./components/Projects";
 import Skills from "./components/Skills";
 import SocialLinks from "./components/SocialLinks";
 
@@ -11,6 +13,8 @@ const App = () => {
       <Home />
       <About />
       <Skills />
+      <Projects />
+      <Contact />
       <SocialLinks />
     </div>
   );
